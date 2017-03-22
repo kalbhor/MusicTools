2d9c6fb68c960fea6a47256ba849fc2e00ceaea8
Lakshay Kalbhor <lakshaykalbhor@gmail.com>
2017-03-22 19:59:37 +0530

    Updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='musictools',
-    version='2.0.2',
+    version='2.1.2',
     description='Lets you repair your music files by adding metadata and album art',
     url='https://github.com/lakshaykalbhor/musictools',
     author='Lakshay Kalbhor',
